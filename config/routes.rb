c1b5d0d46d27cb2417c4d8be3c48d0220495224b
groteck <groteck@sobremesa.(none)>
2012-08-08 23:15:02 +0100

    delete index.html of public and added root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Tracklist::Application.routes.draw do
   resources :albums
 
+  root :to => 'albums#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
