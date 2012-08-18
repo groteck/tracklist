bd5584ba69cd62f57b9f6dd6809d1f2d4b4fafb3
groteck <groteck@sobremesa.(none)>
2012-08-18 13:42:45 +0100

    Delete some files

--- app/models/taglinks.rb
+++
@@ -1,4 +0,0 @@
-class Taglinks < ActiveRecord::Base
-  belongs_to :resource, polymorphic: true
-  belongs_to :tag
-end
--- test/unit/taglinks_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class TaglinksTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
